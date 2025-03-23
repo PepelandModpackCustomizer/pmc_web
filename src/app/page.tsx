972acb5ed839af1b9ddad58e655027af7dc7885d
RashingPro <rashingpro@yandex.ru>
2025-03-23 10:19:11 +0300

    Small fix, remove unused file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
-"use server"
+"use client"
 
 import Header from "@/modules/header.module/module";
-import PageContent from "./page_content";
 import Footer from "@/modules/footer.module/module";
+import Link from "next/link";
+import Image from "next/image";
+import "./style.css"
 
-export default async function Page() {
+function PageContent() {
+    return <div className={"pageContentContainer"}>
+        <div className={"titleContainer"}>
+            <h1 className={"titleHeader"}>Pepeland Modpack Customizer</h1>
+            <h2>
+                <span>Только то, что нужно </span>
+                <span className={"accent"}>тебе</span>
+            </h2>
+            <Link href={"/create"} className={"hover:bg-teal-500 titlePrimaryButton"}>
+                <Image className={"titlePrimaryButtonIcon"} src={"/settings.svg"} alt={"settings icon"} height={1} width={1}/>
+                Создать
+            </Link>
+        </div>
+
+    </div>
+}
+
+export default function Page() {
     return <>
         <Header />
         <PageContent />
-
         <Footer />
     </>
 }
