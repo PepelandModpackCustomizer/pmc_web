ba72d31376841cfa4a5b496b555498861ee73968
RashingPro <rashingpro@yandex.ru>
2025-03-24 14:45:01 +0300

    Fixes, part of footer now visible without scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,22 @@ import "./style.css"
 import AppLayout from "@/modules/AppLayout/module";
 
 function PageContent() {
-    return <div className={"pageContentContainer"}>
-        <div className={"titleContainer"}>
-            <h1 className={"titleHeader"}>Pepeland Modpack Customizer</h1>
-            <h2>
-                <span>Твоя сборка - </span>
-                <span className={"accent"}>твои правила</span>
-            </h2>
-            <Link href={"/create"} className={"titlePrimaryButton"}>
-                <Image className={"titlePrimaryButtonIcon"} src={"/settings.svg"} alt={"settings icon"} height={1} width={1}/>
-                Создать
-            </Link>
-        </div>
+    return <div className={"titleContainer"}>
+        <h1 className={"titleHeader"}>Pepeland Modpack Customizer</h1>
+        <h2>
+            <span>Твоя сборка - </span>
+            <span className={"accent"}>твои правила</span>
+        </h2>
+        <Link href={"/create"} className={"titlePrimaryButton"}>
+            <Image className={"titlePrimaryButtonIcon"} src={"/settings.svg"} alt={"settings icon"} height={1}
+                   width={1}/>
+            Создать
+        </Link>
     </div>
 }
 
 export default function Page() {
     return <AppLayout>
-        <PageContent />
+        <PageContent/>
     </AppLayout>
 }
--- src/modules/Footer/module.tsx
+++ src/modules/Footer/module.tsx
@@ -11,5 +11,9 @@ export default function Module({}: ModuleProps) {
     return <footer className={"footerContainer"}>
         <span>Сайт не является частью сети серверов </span>
         <Link href={"https://pepeland.net/"} className={"accent"}>Pepeland</Link>
+        <br />
+        <span className={"text-zinc-500"}>
+            Not an official Minecraft product. We are in no way affiliated with or endorsed by Mojang Synergies AB, Microsoft Corporation or other rightsholders.
+        </span>
     </footer>
 }
