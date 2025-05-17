8c746dcc050a3198df78980373f8f0c864f8ebc5
RashingPro <rashingpro@yandex.ru>
2025-05-17 19:33:33 +0300

    Add AppLayout, remove unused font

---
+++ src/components/AppLayout/index.tsx
@@ -0,0 +1,13 @@
+"use server"
+
+import Header from "@/components/Header/index"
+import React from "react";
+
+export default async function AppLayout(props: {children?: React.ReactNode}) {
+    return <>
+        <Header />
+        <main className={"root-container"}>
+            {props.children}
+        </main>
+    </>
+}
